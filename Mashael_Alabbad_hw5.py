4793ed2cd50608cc379bd826e017eccfcb568de8
Mashael2 <malabbad1@mail.weber.edu>
2017-03-19 20:28:15 -0600

    Updated

--- Mashael_Alabbad_hw5.py
+++ Mashael_Alabbad_hw5.py
@@ -2,13 +2,38 @@
 import sys
 
 
-# Main function
+def GetInput():
+    """
+    Ask the user for the PIN no more three times
+    The input is 4 digits only 
+    Assume the user's PIN is "1234"
+    """
+    for i in range(3):
+        print("Enter your PIN:")
+        pin = input()
+        # Check the length
+        if len(pin) != 4:
+            print("Invalid PIN length. Correct format is: <9876>")
+        # Check the type
+        elif not pin.isdigit():
+            print("Invalid PIN character. Correct format is: <9876>")  
+        # Check the code
+        elif pin == "1234":
+            print("Your PIN is correct")
+            return
+        print("Your PIN is incorrect")
+        
+    print("Your bank card is blocked")
+    exit(1)        
+            
+
+# Main Function
 def main():
     """
     Test Function.
     """
-
-
+    GetInput()
+    
 
 
 if __name__ == "__main__":
